7ff91360738dda8f75096b44c33d6e2dca4b543e
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-04-20 00:52:06 +0530

    routes for books & users

--- server/index.js
+++ server/index.js
@@ -6,9 +6,14 @@ const app = express();
 require("dotenv").config();
 require("./config/databaseconfig");
 
+const userRoute=require("./routes/userRoute");
+const bookRoute=require("./routes/bookRoute");
+
 
 app.use(cors());
 app.use(express.json());
+app.use("/book",bookRoute);
+app.use("/user",userRoute);
 
 app.listen(8083, () => {
     console.log('Server is running on http://localhost:8083');
